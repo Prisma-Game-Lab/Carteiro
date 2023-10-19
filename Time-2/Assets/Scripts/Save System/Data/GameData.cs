32b904c0f4ca3bfa9c911ff45d2e2efa1a810735
Bbenekko <bbeneko@outlook.com>
2023-10-19 20:28:56 -0300

    Início do savesystem
    Não tem mt coisa para salvar, mas já criei os scripts que criam as funções para salvar o jogo, carregar o jogo e novo jogo, além das interfaces de salvar memória.

---
+++ Time-2/Assets/Scripts/Save System/Data/GameData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+
+public class GameData
+{
+    public string cenaAtualdaSave;
+
+    public GameData() 
+    {
+        this.cenaAtualdaSave = null;
+    }   
+}
