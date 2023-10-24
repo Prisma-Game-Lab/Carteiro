1aa75a1d1cbe1691311c42f0006e8c721da7547c
Vagner Messias <vagnermessias.vmc@gmail.com>
2023-10-24 18:52:19 -0300

    Movimento da Sombra

---
+++ Time-2/Assets/Scripts/ShadowMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowMovement : MonoBehaviour
+{
+    public Transform shadow;
+    public Transform frontPlayer;
+    public Transform ShadowStart;
+
+    [Range(0, 1)] public float ShadowSpeed;
+    [Range(0, 1)] public float ShadowRetreatSpeed;
+
+
+    public float moveValue;
+    public bool startMovement = false;
+
+    private void FixedUpdate()
+    {
+        if(startMovement)
+        {
+            shadow.position = Vector2.Lerp(shadow.position, frontPlayer.position, Time.deltaTime * ShadowSpeed);
+        }
+        else
+        {
+            shadow.position = Vector2.Lerp(shadow.position, ShadowStart.position, Time.deltaTime * ShadowRetreatSpeed);
+        }
+    }
+
+    public void moveShadow(){
+        startMovement = true;
+    }
+}
