a70df61b3feb58b03092899b57cb5dc419e5f04a
Tulio Martins <tulioml240@gmail.com>
2023-10-27 00:51:54 -0300

    botões clicados

--- Time-2/Assets/Scripts/RabiscoScript.cs
+++ Time-2/Assets/Scripts/RabiscoScript.cs
@@ -5,6 +5,10 @@ using UnityEngine.UI;
 
 public class RabiscoScript : MonoBehaviour
 {
+    public int total_buttons;
+    public static int clicked_buttons;
+
+
     public Image imagem;
     // Start is called before the first frame update
     void Start()
@@ -16,13 +20,14 @@ public class RabiscoScript : MonoBehaviour
     {
         if (imagem.isActiveAndEnabled)
         {
-            imagem.enabled = false;
-            Debug.Log("Jair bolsonaro");
+            //imagem.enabled = false;
+            Debug.Log("Ja foi");
         }
         else
         {
             imagem.enabled = true;
-            Debug.Log("Eu só quero...");
+            clicked_buttons++;
+            Debug.Log(clicked_buttons);
         }
     }
 }
