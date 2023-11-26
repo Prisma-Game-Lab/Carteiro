1f3683e67398708bcd8d012caab54235681895af
Tulio Martins <tulioml240@gmail.com>
2023-11-26 02:51:27 -0300

    transição de cartas

--- Time-2/Assets/Scripts/Letter/LetterDisplay.cs
+++ Time-2/Assets/Scripts/Letter/LetterDisplay.cs
@@ -1,26 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LetterDisplay : MonoBehaviour
 {
-    public Letter[] cartas;
+    public LetterList[] cartas;
     public GameObject buttonPrefab;
-    private int current;
-    private int inDisplay;
 
-    public Dictionary<int, List<Object>> botoesDict = new Dictionary<int, List<Object>>();
+    private int currentParagraph; //paragrafo q está carregado no script
+    private int currentLetter; //A carta atual do nivel 
+    private int inDisplay; //paragrafo carregado no display
 
-    public Sprite paragraph;
+    public Dictionary<int, List<Object>> botoesDict = new Dictionary<int, List<Object>>(); //Dicionario dos botoes
 
-    public int num_buttons;
-    public int buttons_pressed;
+    public Sprite paragraph; //Imagem do paragrafo
 
-    public Vector3[] vector3s;
+    [SerializeField]
+    public int num_buttons; //Numero total de botoes da carta atual
+    [SerializeField]
+    public static int buttons_pressed; //Numero de botoes que ja foram pressionados
+
     private GameObject canva;
-
-    //private Button botaoDeTransicao;
 
     //Inicia botões da carta no dicionario
     private void iniciaCartaNoDict(Letter carta)
@@ -37,61 +39,124 @@ public class LetterDisplay : MonoBehaviour
             buttons.Add(button);
         }
 
-        botoesDict.Add(current, buttons);
+        botoesDict.Add(currentParagraph, buttons);
     }
 
     void Start()
     {
-        current = 0; //a q está carregada no script
-        inDisplay = 0; ; //a q está carregada no display
+        currentParagraph = 0;
+        inDisplay = 0; ;
 
-        num_buttons = cartas[inDisplay].num_buttons;
-        buttons_pressed = cartas[inDisplay].buttons_pressed;
+        currentLetter = 0;
+        num_buttons = 0;
 
-        gameObject.GetComponent<Image>().sprite = cartas[inDisplay].paragraph;
+        gameObject.GetComponent<Image>().sprite = cartas[currentLetter].carta[inDisplay].paragraph;
 
         canva = GameObject.Find("Paragraph");
 
-        iniciaCartaNoDict(cartas[current]);
+        iniciaCartaNoDict(cartas[currentLetter].carta[currentParagraph]);
+
+        for(int i = 0; i < cartas[currentLetter].carta.Length; i++)
+        {
+            num_buttons += cartas[currentLetter].carta[i].buttons.Length;
+        }
     }
 
     //Atualiza o indice da carta
-    public void trocaCarta()
+    public void trocaParagrafo()
     {
-        if(current == cartas.Length - 1)
+        if(currentParagraph == cartas.Length - 1)
         {
-            current = 0;
+            currentParagraph = 0;
         }
         else
         {
-            current++;
+            currentParagraph++;
+        }
+    }
+
+    //Atualiza o display no update
+    public void atualizaDisplay()
+    {
+        foreach (GameObject botao in botoesDict[inDisplay])
+        {
+            botao.SetActive(false);
+        }
+
+        if (botoesDict.ContainsKey(currentParagraph))
+        {
+            foreach (GameObject botao in botoesDict[currentParagraph])
+            {
+                botao.SetActive(true);
+            }
+        }
+        else
+        {
+            iniciaCartaNoDict(cartas[currentLetter].carta[currentParagraph]);
+        }
+
+        inDisplay = currentParagraph;
+
+        gameObject.GetComponent<Image>().sprite = cartas[currentLetter].carta[inDisplay].paragraph;
+    }
+
+    //Troca a carta quando todos os botoes forem pressionados
+    public void atualizaCarta()
+    {
+        for(int i = 0; i < cartas[currentLetter].carta.Length; i++)
+        {
+            for (int j = 0; j < cartas[currentLetter].carta.Length; j++) {
+                foreach (GameObject botao in botoesDict[j])
+                {
+                    Destroy(botao);
+                }
+                botoesDict.Remove(j);
+            }
+        }
+
+        currentLetter++;
+
+        currentParagraph = 0;
+        inDisplay = 0; ;
+
+        num_buttons = 0;
+        buttons_pressed = 0;
+
+        gameObject.GetComponent<Image>().sprite = cartas[currentLetter].carta[inDisplay].paragraph;
+
+        canva = GameObject.Find("Paragraph");
+
+        iniciaCartaNoDict(cartas[currentLetter].carta[currentParagraph]);
+
+        for (int i = 0; i < cartas[currentLetter].carta.Length; i++)
+        {
+            num_buttons += cartas[currentLetter].carta[i].buttons.Length;
         }
     }
 
     void Update()
     {
-        if (inDisplay != current)
+        if (inDisplay != currentParagraph && inDisplay + 1 < cartas[currentLetter].carta.Length)
         {
-            foreach (GameObject botao in botoesDict[inDisplay])
-            {
-                botao.SetActive(false);
-            }
+            atualizaDisplay();
+        }
 
-            if (botoesDict.ContainsKey(current))
-            {
-                foreach (GameObject botao in botoesDict[current])
-                {
-                    botao.SetActive(true);
-                }
-            }
-            else
-            {
-                iniciaCartaNoDict(cartas[current]);
-            }
-
-           inDisplay = current;
-
-           gameObject.GetComponent<Image>().sprite = cartas[inDisplay].paragraph;
+        if (buttons_pressed == num_buttons && currentLetter + 1 < cartas.Length)
+        {
+            atualizaCarta();
         }
     }
+
+    public static void pressButton()
+    {
+        buttons_pressed++;
+    }
+}
+
+
+//Classe wraper utilizada para podermos ocupar os arrays a partir do inspector
+[System.Serializable]
+public class LetterList
+{
+    public Letter[] carta;
 }
--- Time-2/Assets/Scripts/Letter/RabiscoScript.cs
+++ Time-2/Assets/Scripts/Letter/RabiscoScript.cs
@@ -5,29 +5,19 @@ using UnityEngine.UI;
 
 public class RabiscoScript : MonoBehaviour
 {
-    public int total_buttons;
-    public static int clicked_buttons;
-
-
-    public Image imagem;
+    private bool pressed;
     // Start is called before the first frame update
     void Start()
     {
-        imagem = GetComponent<Image>();
+        pressed = false;
     }
 
     public void Interruptor()
     {
-        if (imagem.isActiveAndEnabled)
+        if (!pressed)
         {
-            //imagem.enabled = false;
-            Debug.Log("Ja foi");
-        }
-        else
-        {
-            imagem.enabled = true;
-            clicked_buttons++;
-            Debug.Log(clicked_buttons);
+            LetterDisplay.pressButton();
+            pressed = true;
         }
     }
 }
