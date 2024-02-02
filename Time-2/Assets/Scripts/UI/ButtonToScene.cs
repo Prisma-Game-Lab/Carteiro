225c60204e35128d90dca13c278a24caba4b35ce
Vagner Messias <vagnermessias.vmc@gmail.com>
2024-02-02 16:54:31 -0300

    Correcao Bug Configuracoes

--- Time-2/Assets/Scripts/UI/ButtonToScene.cs
+++ Time-2/Assets/Scripts/UI/ButtonToScene.cs
@@ -1,11 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonToScene : MonoBehaviour
 {
     public void goToScene(string name)
     {
+        MenuPrincipalManager.cenaRetorno = SceneManager.GetActiveScene().name;
         SceneTransition.Instance.GoToScene(name);
+    }
+
+    public void backToScene()
+    {
+        SceneTransition.Instance.GoToScene(MenuPrincipalManager.cenaRetorno);
     }
 }
